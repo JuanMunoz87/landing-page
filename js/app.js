9184a72e6f9ac96403126038ecc79e9f3530c046
JuanMunoz87 <jprmd87@gmail.com>
2022-02-02 20:43:26 -0500

    added the scroll funtionality

--- js/app.js
+++ js/app.js
@@ -58,3 +58,40 @@
 // Scroll to section on link click
 
 // Set sections as active
+
+// ======================================================// 
+// References
+const unorderList = document.querySelector("ul");
+console.log(unorderList);
+
+
+//== this will dynamically create a navigation menu ==//
+// array of names
+const navNames = ["Section 1", "Section 2", "Section 3", "Section 4"];
+const datalinks = ["section1", "section2", "section3", "section4"];
+
+// forEach method 
+navNames.forEach(function(arr,i) {
+   //  create a li
+   const newLi = document.createElement("li");
+   // create attribute class and value
+   newLi.setAttribute("class", "menu__link");
+   newLi.setAttribute("data-nav", datalinks[i]);
+   unorderList.appendChild(newLi);
+   newLi.innerHTML = arr;
+});
+// console.log(unorderList);
+
+// Reference
+const menuLinks = document.querySelectorAll(".menu__link");
+// console.log(menuLinks)
+//== scroll to each section of the page ==//
+menuLinks.forEach((item) => {
+   item.addEventListener("click", () => {
+      let el = document.getElementById(item.getAttribute("data-nav"));
+      el.scrollIntoView({behavior:"smooth"});
+      // console.log(el);
+   })
+});
+
+
